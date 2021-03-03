00b31b5810b6ca667e0bfa82a0b272ef0fc9bd42
Fernando Ventura <ventuo01@stepstone.com>
2021-03-02 19:03:34 -0600

    Add project files.

---
+++ WebApp/Configuration/ConfigureCoreServices.cs
@@ -0,0 +1,29 @@
+﻿using ApplicationCore;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Services;
+using Infraestructure.Data;
+using Infraestructure.Loggin;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApp.Configuration
+{
+    public static class ConfigureCoreServices
+    {
+        public static IServiceCollection AddCoreServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(EfRepository<>));
+
+            //Services
+            //services.AddScoped<IBasketService, BasketService>();
+
+            //Repositories
+            //services.AddScoped<IOrderRepository, OrderRepository>();
+
+            services.AddSingleton<IUriComposer>(new UriComposer(configuration.Get<ApplicationSettings>()));
+            services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
+
+            return services;
+        }
+    }
+}
---
+++ WebApp/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace WebApp
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+           //Mapping Here
+           // CreateMap<CatalogItem, CatalogItemDto>();
+        }
+    }
+}
